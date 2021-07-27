6c6d31c4902e50c1555b6ee59a0e5543dacd50ba
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2021-07-26 18:17:11 -0600

    pretty color setup plus working button

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
-const startingMinutes = 10;
-let time = startingMinutes * 60;
+let intervalId;
+let time;
 
 const timeToggle = document.getElementById("timeToggle");
 const countdownEl = document.getElementById("countdown");
+const theBox = document.getElementById("timer");
+
+timeToggle.addEventListener("click", () => {
+  const startingMinutes = document.getElementById("timeSelector").valueAsNumber;
+  time = startingMinutes * 60;
+
+  const button = document.getElementById("timeToggle");
+
+  if (timeToggle.classList.contains("clicked")) {
+    clearInterval(intervalId);
+    button.classList.remove("clicked");
+    theBox.classList.remove("outOfTime");
+    countdownEl.innerHTML = "Mantranome";
+    button.textContent = "Start";
+  } else {
+    updateCountdown();
+    intervalId = setInterval(updateCountdown, 1000);
+    button.classList.add("clicked");
+    button.textContent = "Stop";
+  }
+});
 
 function updateCountdown() {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
+  if (time <= 0) {
+    theBox.classList.add("outOfTime");
+  }
+  const minutes = Math.floor(Math.abs(time) / 60);
+  let seconds = Math.abs(time) % 60;
 
   seconds = seconds < 10 ? "0" + seconds : seconds;
 
   countdownEl.innerHTML = `${minutes}:${seconds}`;
   time--;
 }
-
-timeToggle.addEventListener("click", () => {
-  const button = document.getElementById("timeToggle");
-
-  if (timeToggle.classList.contains("clicked")) {
-    clearInterval(window.refreshIntervalId);
-    button.classList.remove("clicked");
-    countdownEl.innerHTML = "Mantranome";
-    button.textContent = "Start";
-  } else {
-    window.refreshIntervalId = setInterval(updateCountdown, 1000);
-    button.classList.add("clicked");
-    button.textContent = "Stop";
-  }
-});
